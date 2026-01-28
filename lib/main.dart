739826d454f4cc00c488f4287a635e7e8fb263f3
creator88app-ops <creator88.app@gmail.com>
2026-01-28 19:48:47 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:torch_light/torch_light.dart';
+import 'package:battery_plus/battery_plus.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FlashLightApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FlashLightApp extends StatelessWidget {
+  const FlashLightApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'FlashLight Pro',
+      theme: ThemeData.dark(),
+      home: const FlashLightPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class FlashLightPage extends StatefulWidget {
+  const FlashLightPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<FlashLightPage> createState() => _FlashLightPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FlashLightPageState extends State<FlashLightPage> {
+  bool isOn = false;
+  int batteryLevel = 0;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _getBattery();
+  }
+
+  Future<void> _getBattery() async {
+    final battery = Battery();
+    final level = await battery.batteryLevel;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      batteryLevel = level;
     });
+  }
+
+  Future<void> toggleFlash() async {
+    try {
+      if (isOn) {
+        await TorchLight.disableTorch();
+      } else {
+        await TorchLight.enableTorch();
+      }
+      setState(() {
+        isOn = !isOn;
+      });
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: isOn
+                ? [Colors.yellow.shade700, Colors.black]
+                : [Colors.grey.shade900, Colors.black],
+            radius: 0.8,
+          ),
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'FlashLight Pro',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: isOn ? Colors.yellow : Colors.white,
+              ),
+            ),
+            const SizedBox(height: 40),
+            GestureDetector(
+              onTap: toggleFlash,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                width: 180,
+                height: 180,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: isOn ? Colors.yellow : Colors.grey.shade800,
+                  boxShadow: [
+                    BoxShadow(
+                      color: isOn ? Colors.yellowAccent : Colors.black54,
+                      blurRadius: 30,
+                      spreadRadius: 5,
+                    )
+                  ],
+                ),
+                child: Icon(
+                  Icons.power_settings_new,
+                  size: 80,
+                  color: isOn ? Colors.black : Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            Text(
+              'Battery: $batteryLevel%',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Tap the button to toggle flashlight',
+              style: TextStyle(color: Colors.white70),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
